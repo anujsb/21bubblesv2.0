b096c79d2b5919993a07063f87ac540ceaf5da6a
anujsb <anujsbhuyar@gmail.com>
2024-10-07 14:10:13 +0530

    added contact details

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => (
           <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
             Build Next Generation Websites
           </p>
-          {/* <ul className="text-base leading-relaxed text-gray-200 mt-7">
+          <ul className="text-base leading-relaxed text-gray-200 mt-7">
             <li>
               <p className="text-white">Reach out to us via phone:</p>
             </li>
@@ -28,7 +28,7 @@ const Footer = () => (
               <a className="text-gray-200" href="mailto:anujsbhuyar@gmail.com">
               </a>
             </li>
-          </ul> */}
+          </ul>
         </div>
 
         {/* <div className="lg:pl-8">
